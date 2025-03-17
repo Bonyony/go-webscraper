b5584b118cf617be01727762e3254c7e602ec43d
Frank Francione <frankiefrancione@gmail.com>
2025-03-16 20:03:30 -0500

    scraper work

--- cmd/scrape.go
+++ cmd/scrape.go
@@ -5,7 +5,6 @@ package cmd
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/gocolly/colly/v2"
 	"github.com/spf13/cobra"
@@ -41,33 +40,49 @@ func init() {
 	// scrapeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-type Product struct {
-	Name   string `json:"name"`
-	Price  string `json:"price"`
-	ImgUrl string `json:"imgurl"`
-}
+// type Product struct {
+// 	Name   string `json:"name"`
+// 	Price  string `json:"price"`
+// 	ImgUrl string `json:"imgurl"`
+// }
 
 func scrape(cmd *cobra.Command, args []string) {
+	fmt.Println("Scrape command executed!")
+
 	c := colly.NewCollector(
-		colly.AllowedDomains("https://www.sweetwater.com"),
+		// colly.AllowedDomains("www.sweetwater.com"),
+		colly.MaxDepth(1),
 	)
 
-	c.Visit("https://www.sweetwater.com/c590--Solidbody_Guitars")
-
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
+		// These lines pretends to be an internet browser to bypass limiting
+		r.Headers.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+		r.Headers.Set("Accept-Language", "en-US,en;q=0.9")
+		r.Headers.Set("Referer", "https://www.google.com/")
+		r.Headers.Set("DNT", "1") // Do Not Track
+		r.Headers.Set("Connection", "keep-alive")
+		fmt.Println("Visiting", r.URL.String())
 	})
 
-	c.OnError(func(_ *colly.Response, err error) {
-		log.Println("Something went wrong:", err)
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Printf("Status Code %v Error on %s: %s\n", r.StatusCode, r.Request.URL, err)
+		fmt.Println("Response Headers")
+		for key, value := range *r.Headers {
+			fmt.Printf(" %s: %s\n", key, value)
+		}
 	})
 
 	// Find and visit all links
-	c.OnHTML("a", func(e *colly.HTMLElement) {
-		fmt.Println(e.Request.Visit(e.Attr("href")))
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print Link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// c.Visit(e.Request.AbsoluteURL(link))
 	})
 
-	c.OnScraped(func(r *colly.Response) {
-		fmt.Println("Finished", r.Request.URL)
-	})
+	// err := c.Visit("https://www.sweetwater.com/")
+	err := c.Visit("https://en.wikipedia.org/")
+	if err != nil {
+		fmt.Println("Colly error:", err)
+	}
 }
