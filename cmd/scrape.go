e50ea9de6beff338a8e769eef0fde2d479ef841f
Frank Francione <frankiefrancione@gmail.com>
2025-03-20 11:54:48 -0500

    finished basic implementation

--- cmd/scrape.go
+++ cmd/scrape.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly/v2"
 	"github.com/spf13/cobra"
 )
@@ -48,17 +49,117 @@ type Product struct {
 	ImgUrl string `json:"imgurl"`
 }
 
+// maps key value pairs of category names and links
+// var categories = make(map[string]string)
+
 func scrape(cmd *cobra.Command, args []string) {
 	fmt.Println("Scrape command executed!")
 
 	isMusiciansFriend, _ := cmd.Flags().GetBool("musicians-friend")
 
 	if isMusiciansFriend {
-		scrapeMusiciansFriend()
+		scrapeMusiciansFriendSitemap()
 	}
 }
 
-func scrapeMusiciansFriend() {
+func scrapeMusiciansFriendSitemap() {
+	c := colly.NewCollector()
+
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  "*musiciansfriend.com",
+		RandomDelay: 1 * time.Second,
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		// These lines pretends to be an internet browser to bypass limiting
+		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+		r.Headers.Set("Accept-Language", "en-US,en;q=0.9")
+		r.Headers.Set("Referer", "https://www.google.com/")
+		r.Headers.Set("DNT", "1") // Do Not Track
+		r.Headers.Set("Connection", "keep-alive")
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Printf("Status Code %v Error on %s: %s\n", r.StatusCode, r.Request.URL, err)
+		fmt.Println("Response Headers")
+		for key, value := range *r.Headers {
+			fmt.Printf(" %s: %s\n", key, value)
+		}
+	})
+
+	var categoriesList []string
+
+	c.OnHTML("h2.sitemap-heading", func(e *colly.HTMLElement) {
+		categoryName := e.Text
+		if categoryName != "" {
+			categoriesList = append(categoriesList, categoryName)
+		}
+	})
+
+	err := c.Visit("https://www.musiciansfriend.com/sitemap")
+	if err != nil {
+		log.Fatal("Failed to scrape sitemap:", err)
+	}
+
+	fmt.Println("\nChoose a category:")
+	chooseOptionFromList(categoriesList, scrapeMusiciansFriendCategories)
+}
+
+func scrapeMusiciansFriendCategories(category string) {
+	c := colly.NewCollector()
+
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  "*musiciansfriend.com",
+		RandomDelay: 1 * time.Second,
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		// These lines pretends to be an internet browser to bypass limiting
+		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+		r.Headers.Set("Accept-Language", "en-US,en;q=0.9")
+		r.Headers.Set("Referer", "https://www.google.com/")
+		r.Headers.Set("DNT", "1") // Do Not Track
+		r.Headers.Set("Connection", "keep-alive")
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Printf("Status Code %v Error on %s: %s\n", r.StatusCode, r.Request.URL, err)
+		fmt.Println("Response Headers")
+		for key, value := range *r.Headers {
+			fmt.Printf(" %s: %s\n", key, value)
+		}
+	})
+
+	// maps key value pairs of category names and links
+	var categories = make(map[string]string)
+
+	c.OnHTML("h2.sitemap-heading", func(e *colly.HTMLElement) {
+
+		sectionTitle := e.Text
+		if sectionTitle == category {
+			e.DOM.Next().Find("li a").Each(func(i int, s *goquery.Selection) {
+				subCategoryName := s.Text()
+				subCategoryUrl, _ := s.Attr("href")
+
+				if subCategoryName != "" && subCategoryUrl != "" {
+					categories[subCategoryName] = e.Request.AbsoluteURL(subCategoryUrl)
+				}
+			})
+		}
+	})
+
+	err := c.Visit("https://www.musiciansfriend.com/sitemap")
+	if err != nil {
+		log.Fatal("Failed to scrape sitemap:", err)
+	}
+
+	fmt.Println("\nChoose a category:")
+	chooseOptionFromMap(categories, scrapeMusiciansFriendProducts)
+}
+
+func scrapeMusiciansFriendProducts(subCategoryUrl string) {
 	products := make([]Product, 0)
 
 	c := colly.NewCollector(
@@ -91,14 +192,6 @@ func scrapeMusiciansFriend() {
 
 	// Find and visit all links
 	c.OnHTML("div.product-card", func(e *colly.HTMLElement) {
-		fmt.Println("Found a product-card div!")
-		card, err := e.DOM.Html()
-		if err != nil {
-			fmt.Println("Error extracting the HTML")
-			return
-		}
-		fmt.Println("Scraped card HTML:\n", card)
-
 		product := Product{
 			Name:   e.ChildText("a.ui-link"),
 			Price:  e.ChildText(".sale-price"),
@@ -112,7 +205,7 @@ func scrapeMusiciansFriend() {
 		products = append(products, product)
 	})
 
-	err := c.Visit("https://www.musiciansfriend.com/electric-guitars")
+	err := c.Visit(subCategoryUrl)
 	if err != nil {
 		fmt.Println("Colly error:", err)
 	}
@@ -123,4 +216,44 @@ func scrapeMusiciansFriend() {
 	}
 
 	fmt.Println(string(jsonData))
+}
+
+// Prompts the user to input a number corresponding to available choices (takes in a map of strings)
+func chooseOptionFromMap(options map[string]string, callback func(string)) {
+	// loop through and display the options
+	keys := make([]string, 0, len(options))
+	for k := range options {
+		keys = append(keys, k)
+	}
+	for i, name := range keys {
+		fmt.Printf("[%d] %s\n", i+1, name)
+	}
+
+	// get user input (number for category)
+	var choice int
+	fmt.Print("Enter the number of your category: ")
+	fmt.Scan(&choice)
+
+	if choice > 0 && choice <= len(keys) {
+		callback(options[keys[choice-1]])
+	} else {
+		fmt.Println("Invalid choice.")
+	}
+}
+
+// Prompts the user to input a number corresponding to available choices (takes in a list of strings)
+func chooseOptionFromList(options []string, callback func(string)) {
+	for i, name := range options {
+		fmt.Printf("[%d] %s\n", i+1, name)
+	}
+
+	var choice int
+	fmt.Print("Enter the number of your category: ")
+	fmt.Scan(&choice)
+
+	if choice > 0 && choice <= len(options) {
+		callback(options[choice-1])
+	} else {
+		fmt.Println("Invalid choice.")
+	}
 }
