0022220eb0b744cd566bf5f20923f31855a391d5
Frank Francione <frankiefrancione@gmail.com>
2025-04-03 14:51:10 -0500

    added basic status checks

--- cmd/status.go
+++ cmd/status.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"net/url"
 
 	"github.com/spf13/cobra"
@@ -29,25 +30,47 @@ var statusCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(statusCmd)
+
+	statusCmd.Flags().BoolP("parse-url", "p", false, "Parse each part of the URL")
 }
 
 func findStatus(cmd *cobra.Command, args []string) {
+	isParse, _ := cmd.Flags().GetBool("parse-url")
+
 	if len(args) == 0 {
 		fmt.Println("Please provide a URL to check the status of as an argument.\nExample: go-webscraper status https://www.google.com")
 		return
 	}
 
 	for _, domain := range args {
-		getStatus(domain)
+		visitDomain(domain)
+		if isParse {
+			parseURL(domain)
+		}
 	}
+
 }
 
-func getStatus(domain string) {
+func visitDomain(domain string) {
+	// needs the Scheme to properly run, so provide one if there is not?
+	res, err := http.Get(domain)
+	if err != nil {
+		fmt.Println("Connection failed: ", err)
+		return
+	}
+	defer res.Body.Close()
+
+	status := res.Status
+
+	fmt.Println(status, " - ", domain)
+}
+
+func parseURL(domain string) {
 	u, err := url.Parse(domain)
 	if err != nil {
 		log.Println("Unable to parse rawURL:", err)
 		return
 	}
 
-	fmt.Println("Scheme:", u.Scheme, "Host:", u.Host, "Path:", u.Path)
+	fmt.Print("Scheme:", u.Scheme, "Host:", u.Host, "Path:", u.Path, "\n\n")
 }
